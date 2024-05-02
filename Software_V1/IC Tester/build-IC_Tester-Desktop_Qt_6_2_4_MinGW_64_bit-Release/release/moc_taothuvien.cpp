ad171bb4147991b690a41c68fa46d4ba006608ed
Bui-Diep <buidiepmta01@gmail.com>
2024-05-02 14:33:03 +0700

    update Software

---
+++ Software_V1/IC Tester/build-IC_Tester-Desktop_Qt_6_2_4_MinGW_64_bit-Release/release/moc_taothuvien.cpp
@@ -0,0 +1,268 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'taothuvien.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.2.4)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../../../IC Tester/IC_Tester/include/taothuvien.h"
+#include <QtGui/qtextcursor.h>
+#include <QScreen>
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'taothuvien.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.2.4. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MyButton_t {
+    const uint offsetsAndSize[8];
+    char stringdata0[23];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(offsetof(qt_meta_stringdata_MyButton_t, stringdata0) + ofs), len 
+static const qt_meta_stringdata_MyButton_t qt_meta_stringdata_MyButton = {
+    {
+QT_MOC_LITERAL(0, 8), // "MyButton"
+QT_MOC_LITERAL(9, 7), // "entered"
+QT_MOC_LITERAL(17, 0), // ""
+QT_MOC_LITERAL(18, 4) // "left"
+
+    },
+    "MyButton\0entered\0\0left"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MyButton[] = {
+
+ // content:
+      10,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       2,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       2,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   26,    2, 0x06,    1 /* Public */,
+       3,    0,   27,    2, 0x06,    2 /* Public */,
+
+ // signals: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MyButton::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MyButton *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->entered(); break;
+        case 1: _t->left(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (MyButton::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MyButton::entered)) {
+                *result = 0;
+                return;
+            }
+        }
+        {
+            using _t = void (MyButton::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MyButton::left)) {
+                *result = 1;
+                return;
+            }
+        }
+    }
+    (void)_a;
+}
+
+const QMetaObject MyButton::staticMetaObject = { {
+    QMetaObject::SuperData::link<QPushButton::staticMetaObject>(),
+    qt_meta_stringdata_MyButton.offsetsAndSize,
+    qt_meta_data_MyButton,
+    qt_static_metacall,
+    nullptr,
+qt_incomplete_metaTypeArray<qt_meta_stringdata_MyButton_t
+, QtPrivate::TypeAndForceComplete<MyButton, std::true_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
+
+
+
+>,
+    nullptr
+} };
+
+
+const QMetaObject *MyButton::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MyButton::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MyButton.stringdata0))
+        return static_cast<void*>(this);
+    return QPushButton::qt_metacast(_clname);
+}
+
+int MyButton::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QPushButton::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 2)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 2;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void MyButton::entered()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, nullptr);
+}
+
+// SIGNAL 1
+void MyButton::left()
+{
+    QMetaObject::activate(this, &staticMetaObject, 1, nullptr);
+}
+struct qt_meta_stringdata_TaoThuVien_t {
+    const uint offsetsAndSize[16];
+    char stringdata0[91];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(offsetof(qt_meta_stringdata_TaoThuVien_t, stringdata0) + ofs), len 
+static const qt_meta_stringdata_TaoThuVien_t qt_meta_stringdata_TaoThuVien = {
+    {
+QT_MOC_LITERAL(0, 10), // "TaoThuVien"
+QT_MOC_LITERAL(11, 20), // "opTrangCauHinhDuLieu"
+QT_MOC_LITERAL(32, 0), // ""
+QT_MOC_LITERAL(33, 18), // "opTrangCauHinhChan"
+QT_MOC_LITERAL(52, 10), // "opComplete"
+QT_MOC_LITERAL(63, 6), // "dienAp"
+QT_MOC_LITERAL(70, 6), // "soChan"
+QT_MOC_LITERAL(77, 13) // "taoFileDuLieu"
+
+    },
+    "TaoThuVien\0opTrangCauHinhDuLieu\0\0"
+    "opTrangCauHinhChan\0opComplete\0dienAp\0"
+    "soChan\0taoFileDuLieu"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_TaoThuVien[] = {
+
+ // content:
+      10,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       4,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   38,    2, 0x0a,    1 /* Public */,
+       3,    0,   39,    2, 0x0a,    2 /* Public */,
+       4,    2,   40,    2, 0x0a,    3 /* Public */,
+       7,    1,   45,    2, 0x0a,    6 /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString, QMetaType::Int,    5,    6,
+    QMetaType::Void, QMetaType::QString,    6,
+
+       0        // eod
+};
+
+void TaoThuVien::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<TaoThuVien *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->opTrangCauHinhDuLieu(); break;
+        case 1: _t->opTrangCauHinhChan(); break;
+        case 2: _t->opComplete((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<int>>(_a[2]))); break;
+        case 3: _t->taoFileDuLieu((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1]))); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject TaoThuVien::staticMetaObject = { {
+    QMetaObject::SuperData::link<QWidget::staticMetaObject>(),
+    qt_meta_stringdata_TaoThuVien.offsetsAndSize,
+    qt_meta_data_TaoThuVien,
+    qt_static_metacall,
+    nullptr,
+qt_incomplete_metaTypeArray<qt_meta_stringdata_TaoThuVien_t
+, QtPrivate::TypeAndForceComplete<TaoThuVien, std::true_type>
+, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<const QString &, std::false_type>, QtPrivate::TypeAndForceComplete<const int &, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<const QString &, std::false_type>
+
+
+>,
+    nullptr
+} };
+
+
+const QMetaObject *TaoThuVien::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *TaoThuVien::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_TaoThuVien.stringdata0))
+        return static_cast<void*>(this);
+    return QWidget::qt_metacast(_clname);
+}
+
+int TaoThuVien::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 4)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 4;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 4)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 4;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
